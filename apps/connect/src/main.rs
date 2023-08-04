21b4bb3fc001f09911a34f659164eef2a3048d02
Shi Lei <shi_lei@massclouds.com>
2023-08-04 07:37:01 +0000

    apps: minor fixes for connect

--- apps/connect/src/main.rs
+++ apps/connect/src/main.rs
@@ -1,10 +1,8 @@
-//! An example of hooking up stdin/stdout to either a TCP or UDP stream.
+//! An example of hooking up stdin/stdout to either a TCP stream.
 //!
 //! This example will connect to a socket address specified in the argument list
 //! and then forward all data read on stdin to the server, printing out all data
-//! received on stdout. An optional `--udp` argument can be passed to specify
-//! that the connection should be made over UDP instead of TCP, translating each
-//! line entered on stdin to a UDP packet to be sent to the remote address.
+//! received on stdout.
 //!
 //! Note that this is not currently optimized for performance, especially
 //! around buffer management. Rather it's intended to show an example of
@@ -29,25 +27,13 @@
 
 use futures::StreamExt;
 use tokio::io;
-use std::io::Read;
 use tokio_util::codec::{BytesCodec, FramedRead, FramedWrite};
 
-use std::env;
 use std::error::Error;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Determine if we're going to run in TCP or UDP mode
-    let mut args = env::args().skip(1).collect::<Vec<_>>();
-    let tcp = match args.iter().position(|a| a == "--udp") {
-        Some(i) => {
-            args.remove(i);
-            false
-        }
-        None => true,
-    };
-
     // Parse what address we're going to connect to
     let addr = "192.168.0.60:6142".to_string();
     let addr = addr.parse::<SocketAddr>()?;
@@ -56,29 +42,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let stdin = stdin.map(|i| i.map(|bytes| bytes.freeze()));
     let stdout = FramedWrite::new(io::stdout(), BytesCodec::new());
 
-    println!("###### 1... ######");
-    /*
-    loop {
-        let mut buf: [u8; 2] = [0; 2];
-        if std::io::stdin().read(&mut buf).ok() != Some(1) {
-    println!("###### 1.1... ######");
-            continue;
-        }
-        print!("{:?}", buf);
-        println!("");
-    }
-    println!("###### 2... ######");
-    */
     tcp::connect(&addr, stdin, stdout).await?;
-    println!("###### OK! ######");
-    /*
-    if tcp {
-        tcp::connect(&addr, stdin, stdout).await?;
-    } else {
-        udp::connect(&addr, stdin, stdout).await?;
-    }
-    */
-
     Ok(())
 }
 
@@ -94,9 +58,7 @@ mod tcp {
         mut stdin: impl Stream<Item = Result<Bytes, io::Error>> + Unpin,
         mut stdout: impl Sink<Bytes, Error = io::Error> + Unpin,
     ) -> Result<(), Box<dyn Error>> {
-        println!("before connect addr {:?}", addr);
         let mut stream = TcpStream::connect(addr).await?;
-        println!("after connect addr {:?}", addr);
         let (r, w) = stream.split();
         let mut sink = FramedWrite::new(w, BytesCodec::new());
         // filter map Result<BytesMut, Error> stream into just a Bytes stream to match stdout Sink
@@ -112,68 +74,9 @@ mod tcp {
             })
             .map(Ok);
 
-        println!("============ step3");
         match future::join(sink.send_all(&mut stdin), stdout.send_all(&mut stream)).await {
             (Err(e), _) | (_, Err(e)) => Err(e.into()),
             _ => Ok(()),
         }
     }
 }
-
-/*
-mod udp {
-    use bytes::Bytes;
-    use futures::{Sink, SinkExt, Stream, StreamExt};
-    use std::error::Error;
-    use std::io;
-    use std::net::SocketAddr;
-    use tokio::net::UdpSocket;
-
-    pub async fn connect(
-        addr: &SocketAddr,
-        stdin: impl Stream<Item = Result<Bytes, io::Error>> + Unpin,
-        stdout: impl Sink<Bytes, Error = io::Error> + Unpin,
-    ) -> Result<(), Box<dyn Error>> {
-        // We'll bind our UDP socket to a local IP/port, but for now we
-        // basically let the OS pick both of those.
-        let bind_addr = if addr.ip().is_ipv4() {
-            "0.0.0.0:0"
-        } else {
-            "[::]:0"
-        };
-
-        let socket = UdpSocket::bind(&bind_addr).await?;
-        socket.connect(addr).await?;
-
-        tokio::try_join!(send(stdin, &socket), recv(stdout, &socket))?;
-
-        Ok(())
-    }
-
-    async fn send(
-        mut stdin: impl Stream<Item = Result<Bytes, io::Error>> + Unpin,
-        writer: &UdpSocket,
-    ) -> Result<(), io::Error> {
-        while let Some(item) = stdin.next().await {
-            let buf = item?;
-            writer.send(&buf[..]).await?;
-        }
-
-        Ok(())
-    }
-
-    async fn recv(
-        mut stdout: impl Sink<Bytes, Error = io::Error> + Unpin,
-        reader: &UdpSocket,
-    ) -> Result<(), io::Error> {
-        loop {
-            let mut buf = vec![0; 1024];
-            let n = reader.recv(&mut buf[..]).await?;
-
-            if n > 0 {
-                stdout.send(Bytes::from(buf)).await?;
-            }
-        }
-    }
-}
-*/
